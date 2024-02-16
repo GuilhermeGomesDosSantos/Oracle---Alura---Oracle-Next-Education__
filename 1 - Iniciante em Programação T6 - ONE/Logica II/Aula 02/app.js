73a9cd7303ed4e820680b73b67da7b0cdfa470ed
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-02-16 20:10:21 -0300

    Aula 02 - Criando uma função

---
+++ 1 - Iniciante em Programação T6 - ONE/Logica II/Aula 02/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto'
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um Número entre 1 e 10'
+
+
+function verificarChute () {
+    console.log('Botão chutar foi clicado')
+}
