525c1432f50c965a691119ca77a6b5a517a23a91
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-02-16 19:59:39 -0300

    Aula 11 - Math.random

---
+++ 1 - Iniciante em Programação T6 - ONE/Logica I/Aula 11/index.js
@@ -0,0 +1,5 @@
+// let aleatorio = Math.random ()
+// let aleatorio = Math.random () * 10
+// let aleatorio = parseInt(Math.random () * 10)// paeseInt retorna um valor inteiro
+let aleatorio = parseInt(Math.random() * 10 + 1)// se não acrescentar + 1, irá retornar de 0 até 9
+console.log(aleatorio)
