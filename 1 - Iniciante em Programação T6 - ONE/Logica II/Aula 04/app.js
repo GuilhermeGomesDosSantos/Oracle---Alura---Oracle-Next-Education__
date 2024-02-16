65c37ff2641636284898a65dbff950d617df0a21
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-02-16 20:12:07 -0300

    Aula 04 - Funções com Retorno

---
+++ 1 - Iniciante em Programação T6 - ONE/Logica II/Aula 04/app.js
@@ -0,0 +1,23 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do Número Secreto'
+
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um Número entre 1 e 10'
+
+let numeroSecreto = gerarNumeroAleatorio()
+
+function verificarChute () {
+    console.log(numeroSecreto)
+}
+
+function exibirTextoNaTela (tag, texto) {
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto')
+exibirTextoNaTela('p', 'Escolha um Número de 1 a 10')
+
+function gerarNumeroAleatorio () {
+    return parseInt(Math.random() * 10 + 1)
+}
