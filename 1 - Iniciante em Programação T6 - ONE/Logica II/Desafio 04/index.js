99b5b317b2785c46cbed1156033b687279f49e94
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-02-16 20:21:59 -0300

    Quarto Desafio

---
+++ 1 - Iniciante em Programação T6 - ONE/Logica II/Desafio 04/index.js
@@ -0,0 +1,25 @@
+/*
+Crie uma lista vazia, com o nome listaGenerica.
+Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+*/
+
+let listaGenerica = [];
+
+console.log(listaGenerica)
+let linguagensDeProgramacao = ['JavaScript','C','C++','Kotlin','Python'];
+linguagensDeProgramacao.push('Java');
+console.log(linguagensDeProgramacao);
+linguagensDeProgramacao.push('Ruby');
+console.log(linguagensDeProgramacao);
+linguagensDeProgramacao.push('GoLang');
+console.log(linguagensDeProgramacao);
+
+let nomes = ['Guilherme', 'GDS', 'Gui']
+console.log(nomes[0]);
+console.log(nomes[1]);
+console.log(nomes[2]);
+console.log(nomes[3]);
