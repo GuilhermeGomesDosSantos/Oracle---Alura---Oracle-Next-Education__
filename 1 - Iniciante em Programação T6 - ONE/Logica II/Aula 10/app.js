65eafbbac87a57d13e0bb342c0273595e3856025
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-02-16 20:16:52 -0300

    Aula 10 - Documentação sobre listas

---
+++ 1 - Iniciante em Programação T6 - ONE/Logica II/Aula 10/app.js
@@ -0,0 +1,13 @@
+// Lista
+
+let numeros = [1, 5, 9]
+console.log(numeros)
+console.log(numeros.length)
+console.log(numeros[0])
+
+let linguagens = ['Javascript', 'Java', 'Python']
+console.log(linguagens[1])
+console.log(linguagens[2])
+console.log(linguagens[90]) // caso não tenha uma valor no indice que foi solicitado, irá sempre retornar undefined
+
+console.log(linguagens[linguagens.length -1])
